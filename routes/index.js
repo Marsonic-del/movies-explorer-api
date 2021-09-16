4bd912a24e28445234467b37b75cc9785181a27c
Vladimir Marsov <marsov012@gmail.com>
2021-09-16 14:10:16 +0300

    cors

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,10 @@ const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+    console.log('allowed');
   } else {
     corsOptions = { origin: false }; // disable CORS for this request
+    console.log('not allowed');
   }
   callback(null, corsOptions); // callback expects two parameters: error and options
 };
